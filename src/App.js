6eaffaf6d3c2aea336a2792cc573a8842d4e39b7
Alexander Torres <alexander.torres@quanticoservicios.com>
2023-10-23 15:32:35 -0400

    cambios admin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import CreateRoute from './components/CreateRoute/CreateRoute';
+
+import Selection from './components/Selection/Selection';
+import CreateCompany from './CreateCompany/CreateCompany';
+import CreateBus from './CreateBus/CreateBus';
+import ModifyCompany from './ModifyCompany/ModifyCompany';
+import ModifyBus from './ModifyBus/ModifyBus';
+import ModifyRoute from './ModifyRoute/ModifyRoute';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route exact path='/' element={<Selection />} />
+          <Route path='/create_company' element={<CreateCompany />} />
+          <Route path='/create_bus' element={<CreateBus />} />
+          <Route path='/create_route' element={<CreateRoute />} />
+          <Route path='/modify_company' element={<ModifyCompany />} />
+          <Route path='/modify_bus' element={<ModifyBus />} />
+          <Route path='/modify_route' element={<ModifyRoute />} />
+        </Routes>
+      </div>
   );
 }
 
