eadaa883f4937bee017ea555edb761d8e6872c4c
Melissa Lopez <melilo89@gmail.com>
2023-10-28 09:36:01 -0300

    login adm

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import { Route, Routes } from 'react-router-dom';
-import CreateRoute from './components/CreateRoute/CreateRoute';
+import { Route, Routes } from "react-router-dom";
+import CreateRoute from "./components/CreateRoute/CreateRoute";
 
-import Selection from './components/Selection/Selection';
-import CreateCompany from '../src/components/CreateCompany/CreateCompany'
-import CreateBus from '../src/components/CreateBus/CreateBus';
-import ModifyCompany from '../src/components/ModifyCompany/ModifyCompany';
-import ModifyBus from '../src/components/ModifyBus/ModifyBus';
-import ModifyRoute from '../src/components/ModifyRoute/ModifyRoute';
-import './App.css';
-import Login from './components/Login/Login';
+import Selection from "./components/Selection/Selection";
+import CreateCompany from "../src/components/CreateCompany/CreateCompany";
+import CreateBus from "../src/components/CreateBus/CreateBus";
+import ModifyCompany from "../src/components/ModifyCompany/ModifyCompany";
+import ModifyBus from "../src/components/ModifyBus/ModifyBus";
+import ModifyRoute from "../src/components/ModifyRoute/ModifyRoute";
+import "./App.css";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
-      <div className="App">
-        <Routes>
-                    <Route path='/login' element={<Login />} />
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<Login />} />
 
-          <Route exact path='/' element={<Selection />} />
-          <Route path='/create_company' element={<CreateCompany />} />
-          <Route path='/create_bus' element={<CreateBus />} /> 
-          <Route path='/create_route' element={<CreateRoute />} />
-          <Route path='/modify_company' element={<ModifyCompany />} />
-          <Route path='/modify_bus' element={<ModifyBus />} />
-          <Route path='/modify_route' element={<ModifyRoute />} />
-        </Routes>
-      </div>
+        <Route exact path="/" element={<Selection />} />
+        <Route path="/create_company" element={<CreateCompany />} />
+        <Route path="/create_bus" element={<CreateBus />} />
+        <Route path="/create_route" element={<CreateRoute />} />
+        <Route path="/modify_company" element={<ModifyCompany />} />
+        <Route path="/modify_bus" element={<ModifyBus />} />
+        <Route path="/modify_route" element={<ModifyRoute />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,56 +1,149 @@
-// import styles from '../Login/Login.module.css';
-
-import React from "react";
-import "../Login/login.css"
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import authLayout from "../Login/authLayout";
+import "../Login/login.css";
+// import { useDispatch } from "react-redux";
+import axios from "axios";
+import Swal from "sweetalert2";
+import Cookies from "js-cookie";
 
-class LoginPage extends React.Component {
-    constructor(props){
-        super(props);
+const LoginPage = (props) => {
 
-        this.state = {};
+    // const dispatch = useDispatch();
+
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState("");
+  // const [show, setShow] = useState(false);
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+
+
+
+ const handleSubmit = async (event) => {
+  event.preventDefault();
+  const { email, password } = userData;
+
+  try {
+    const { data } = await axios.post("http://localhost:3001/auth/login", {
+      correo: email,
+      password: password,
+    });
+    console.log(data, 'DATA');
+
+    if (data.error) {
+      Swal.fire({
+        title: data.error,
+        icon: "error",
+      });
+    } else if (data.token) {
+      if (data.data && data.data.type === "admin") {
+        setToken(data.token);
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("nombre", data.data.nombre);
+        localStorage.setItem("apellido", data.data.apellido);
+        localStorage.setItem("correo", data.data.correo);
+        window.location.reload();
+
+        // window.location.href = "/home"; 
+      } else {
+        Swal.fire({
+          title: "Acceso denegado. No eres administrador.",
+          icon: "error",
+        });
+      }
     }
+  } catch (error) {
+    console.log('no se pudo procesar el logueo');
+  }
+};
 
-    render(){
-        return <>
-            <form className="login-form">
-                <div className="d-flex align-items-center my-4">
-                    <h1 className="text-center fw-normal mb-0 me-3">Sign In</h1>
-                </div>
-                {/* <!-- Email input --> */}
-                <div className="form-outline mb-4">
-                    <label className="form-label" htmlFor="form3Example3">Email address</label>
-                    <input type="email" id="form3Example3" className="form-control form-control-lg"
-                    placeholder="Enter a valid email address" />
-                </div>
+  
 
-                {/* <!-- Password input --> */}
-                <div className="form-outline mb-3">
-                    <label className="form-label" htmlFor="form3Example4">Password</label>
-                    <input type="password" id="form3Example4" className="form-control form-control-lg"
-                    placeholder="Enter password" />
-                </div>
-
-                <div className="d-flex justify-content-between align-items-center">
-                    {/* <!-- Checkbox --> */}
-                    <div className="form-check mb-0">
-                    <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                    <label className="form-check-label" htmlFor="form2Example3">
-                        Remember me
-                    </label>
-                    </div>
-                    <Link to="/reset-password" className="text-body">Forgot password?</Link>
-                </div>
-
-                <div className="text-center text-lg-start mt-4 pt-2">
-                    <Link to="/" type="button" className="btn btn-primary btn-lg">Login</Link>
-                    <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
-                        className="link-danger">Register</a></p>
-                </div>
-            </form>
-        </>
+  useEffect(() => {
+    const userData = Cookies.get("userData");
+    if (userData) {
+      const parsedUser = JSON.parse(userData);
+      setUser(parsedUser);
     }
-}
+  }, []);
+
+  return (
+    console.log(userData, 'USERDATA'),
+    <>
+      {/* <form className="login-form" onSubmit={handleSubmit}> */}
+      <form className="login-form">
+        <div className="d-flex align-items-center my-4">
+          <h1 className="text-center fw-normal mb-0 me-3">Sign In</h1>
+        </div>
+        {/* Email input */}
+        <div className="form-outline mb-4">
+          <label className="form-label" htmlFor="form3Example3" label="Email">
+            Email address
+          </label>
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(event) =>
+              setUserData({ ...userData, email: event.target.value })
+            }
+            id="form3Example3"
+            className="form-control form-control-lg"
+          />
+      {error && <p className="error">Este es un mensaje de error.</p>}
+        </div>
+
+        {/* Password input */}
+        <div className="form-outline mb-3">
+          <label className="form-label" htmlFor="form3Example4">
+            Password
+          </label>
+          <input
+            type="password"
+            id="form3Example4"
+            className="form-control form-control-lg"
+            placeholder="Contraseña"
+            onChange={(event) => setUserData({ ...userData, password: event.target.value })}
+          />
+        </div>
+
+        <div className="d-flex justify-content-between align-items-center">
+          {/* Checkbox */}
+          <div className="form-check mb-0">
+            <input
+              className="form-check-input me-2"
+              type="checkbox"
+              value=""
+              id="form2Example3"
+            
+            />
+            <label className="form-check-label" htmlFor="form2Example3">
+              Remember me
+            </label>
+          </div>
+          <Link to="/reset-password" className="text-body">
+            Forgot password?
+          </Link>
+        </div>
+
+        <div className="text-center text-lg-start mt-4 pt-2">
+        <Link to="/">
+        <button type="submit" className="btn btn-primary btn-lg" onClick={handleSubmit}>
+          Login
+        </button>
+      </Link>
+          <p className="small fw-bold mt-2 pt-1 mb-0">
+            Don't have an account?{" "}
+            <a href="#!" className="link-danger">
+              Register
+            </a>
+          </p>
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default authLayout(LoginPage);
