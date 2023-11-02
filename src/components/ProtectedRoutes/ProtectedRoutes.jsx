b733b99b80e829b7f070ee348c4e47404ad9e54c
MatiTar <Matiastari@outlook.com.ar>
2023-11-02 12:56:42 -0300

    Home con conexion a los usuarios

--- src/components/ProtectedRoutes/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
-// const ProtectedRoutes = ({ }) => {
-//     if (!localStorage.getItem('token')) {
-//         return <Navigate to="/" />;
-//     }
-//     return <div><Outlet /></div>;
-// };
 const ProtectedRoutes = ({ }) => {
-    return <Outlet />;
+    if (!localStorage.getItem('token')) {
+        return <Navigate to="/" />;
+    }
+    return <div><Outlet /></div>;
 };
 
 export default ProtectedRoutes
