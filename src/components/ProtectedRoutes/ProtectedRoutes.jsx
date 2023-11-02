2b8a95e9c94fbf780aa0073625f444e0b80c7ece
alexander torres <alexander20012@hotmail.com>
2023-11-02 15:28:18 -0400

    cambios

--- src/components/ProtectedRoutes/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -1,29 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import Cookies from 'js-cookie';
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
 
-const ProtectedRoutes = () => {
-    const [userGoogle, setUserGoogle] = useState(null);
-
-    useEffect(() => {
-        const userData = Cookies.get("userData");
-
-        if (userData) {
-            const parsedUser = JSON.parse(userData);
-            console.log("parsedUser:", parsedUser);
-            setUserGoogle(parsedUser);
-        }
-    }, []);
-
-    // Utiliza un useEffect separado para manejar lógica posterior a la actualización del estado
-    useEffect(() => {
-        if (!localStorage.getItem('token') || !userGoogle) {
-            // Realiza una redirección u ejecuta la lógica que necesites aquí
-            // Puedes confiar en que userGoogle se ha actualizado en este punto
-        }
-    }, [userGoogle]);
-
+const ProtectedRoutes = ({ }) => {
+    if (!localStorage.getItem('token')) {
+        return <Navigate to="/" />;
+    }
     return <div><Outlet /></div>;
 };
 
-export default ProtectedRoutes;
+export default ProtectedRoutes
